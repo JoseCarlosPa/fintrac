5caa29d75e7ed034c6059ff25a0c78e6b4080674
josecarlos <josecarlos@leadsales.io>
2024-03-09 09:58:56 -0600

    feat(cards): add cards page and refactor styles

--- src/app/home/cards/page.tsx
+++ src/app/home/cards/page.tsx
@@ -7,16 +7,18 @@ import CardExample from "@/app/home/cards/components/CardExample";
 
 const CreditCards = () => {
 
-  const [cards, setCards] = useState<any>([])
+  const [cards, setCards] = useState<Array<any>>([])
 
 
   const getCreditCards = async () => {
+    setCards([])
     auth.onAuthStateChanged((user) => {
       if (user === null) return
       const creditCardsArray = query(collection(db, "users", user.uid, "credit_cards"), orderBy('name', 'asc'))
       const creditCardsSnapshot = getDocs(creditCardsArray)
       creditCardsSnapshot.then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
+
           setCards([...cards, doc.data()])
         });
       });
@@ -31,7 +33,7 @@ const CreditCards = () => {
     <div className="flex flex-col">
       <span className="font-bold text-lg">Mis tarjetas</span>
       <div className="grid grid-cols-12 mt-12">
-        {cards?.lenght > 0 ?
+        {cards?.length > 0 ?
           <div className="col-span-12  md:col-span-4">
             {cards.map((card: any, index: number) => {
               return <CreditCard key={index} number={card.number} expiryDate={card.expiryDate} isVisa={card.isVisa}
