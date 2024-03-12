aab3c9ce558843fc000a9785160352237a65eb78
josecarlos <josecarlos@leadsales.io>
2024-03-12 12:46:57 -0600

    fix(budget):add budget quincenal

---
+++ src/app/home/budgets/components/modals/BudgetConfigModal.tsx
@@ -0,0 +1,55 @@
+"use client"
+import Modal from "@/app/componentes/Modal";
+import {db, auth} from "@/firebase";
+import {useState} from "react";
+import {doc, updateDoc} from "firebase/firestore";
+
+type BudgetConfigModalProps = {
+  show: boolean;
+  onClose: () => void;
+
+
+}
+const BudgetConfigModal = ({show,onClose}:BudgetConfigModalProps) => {
+
+  const [payload, setPayload] = useState({
+    fortnight: 0,
+  })
+
+  const handleSave = async()=> {
+    auth.onAuthStateChanged(async (user) => {
+      if (user === null) return
+      const userRef = doc(db, 'users', user.uid)
+      await updateDoc(userRef, {
+        fortnight: payload.fortnight
+      })
+    })
+  }
+
+  return(
+    <Modal show={show} onClose={onClose} >
+      <div className="flex flex-col p-4">
+        <div className="felx flex-row justify-center">
+          <span>Configuración</span>
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="amount">¿Cuanto ganas a la Quincena?</label>
+          <input type="number" min={0}
+            value={payload.fortnight}
+                 onChange={(e) => setPayload({...payload, fortnight: parseInt(e.target.value)})}
+                  className="border border-gray-400 rounded-md p-2" />
+        </div>
+        <div className="flex flex-row justify-center gap-x-4">
+          <button
+            onClick={handleSave}
+            className="bg-gray-900 hover:bg-gray-800 text-white rounded-md px-4 py-2">Guardar</button>
+          <button onClick={onClose} className="bg-gray-900 hover:bg-gray-800 text-white rounded-md px-4 py-2">Cancelar</button>
+
+        </div>
+
+      </div>
+    </Modal>
+  );
+}
+
+export default BudgetConfigModal;
--- src/app/home/budgets/page.tsx
+++ src/app/home/budgets/page.tsx
@@ -1,22 +1,18 @@
 "use client"
-import {useEffect, useRef, useState} from "react";
-import {db, auth} from "@/firebase";
-import {collection, query, getDocs, orderBy, doc, updateDoc} from "firebase/firestore";
+import {useEffect, useState} from "react";
+import {auth, db} from "@/firebase";
+import {collection, doc, getDocs, orderBy, query, updateDoc} from "firebase/firestore";
 import {Budget} from "@/types/Budget";
-import {Card} from "@/types/Card";
 import BudgetModal from "@/app/home/budgets/components/modals/BudgetModal";
 import {FaGear} from "react-icons/fa6";
+import BudgetConfigModal from "@/app/home/budgets/components/modals/BudgetConfigModal";
 
 const BudgetsPage = () => {
 
   const [open, setOpen] = useState(false)
+  const [openGear, setOpenGear] = useState(false)
   const [budgets, setBudgets] = useState<any[]>([])
   const [creditCards, setCreditCards] = useState<any[]>([])
-  const [creditCardsTotal, setCreditCardsTotal] = useState(0);
-
-  const handleCreditCardsTotalChange = (total: number) => {
-    setCreditCardsTotal(prevState => prevState + total);
-  };
 
   const getBudgets = async () => {
     setBudgets([])
@@ -66,24 +62,10 @@ const BudgetsPage = () => {
     })
   }
 
-  const orderedCreditCards = () => {
-    return creditCards.sort((a:Card,b:Card) => {
-      if(parseInt(a.cut_date) > parseInt(b.cut_date)) return 1
-      if(parseInt(a.cut_date) < parseInt(b.cut_date)) return -1
-      return 0
-    })
-  }
-
   const calculateTotal = () => {
-    // Calcula el total de los presupuestos
-    const totalBudgets = budgets.reduce((acc, budget) => {
+    return budgets.reduce((acc, budget) => {
       return acc + budget.amount;
     }, 0);
-
-    // Suma el total de las tarjetas de crédito al total de los presupuestos
-    const total = totalBudgets + creditCardsTotal;
-
-    return total;
   };
 
   const handleIsPaid = async (budget: Budget) => {
@@ -94,8 +76,6 @@ const BudgetsPage = () => {
         paid: !budget.paid
       }
       await updateDoc(budgetRef, payload).then(() => {
-        console.log('handleIsPaid', budgetRef, payload)
-
         setBudgets((prevBudgets) => {
           return prevBudgets.map((prevBudget) => {
             if (prevBudget.id === budget.id) {
@@ -109,77 +89,85 @@ const BudgetsPage = () => {
     })
   }
 
-
   return(
-      <div className="flex flex-col">
-        {open && <BudgetModal setBudgets={setBudgets} creditCards={creditCards} open={open} onClose={() => {
-          setOpen(false)
-        }}/>}
-        <div className="flex flex-row">
-          <span className="font-bold text-xl">Presupuesto</span>
-        </div>
-        <div className="flex flex-row justify-end gap-x-4 mt-12">
-          <button
-              onClick={() => setOpen(true)}
-              className="bg-gray-900 hover:bg-gray-800 text-white rounded-md px-4 py-2"
-          >
-            <FaGear className="text-white w-6 h-6"/>
-          </button>
-          <button
-              onClick={() => setOpen(true)}
-              className="bg-gray-900 hover:bg-gray-800 text-white rounded-md px-4 py-2"
-          >
-            + Agregar presupuesto
-          </button>
-        </div>
-        <div className="flex flex-row mt-12">
-          <table className="w-full">
-            <thead>
-            <tr>
-              <th className="px-2 py-2 text-sm border border-gray-400">Nombre</th>
-              <th className="px-2 py-2 text-sm border border-gray-400">Monto</th>
-              <th className="px-2 py-2 text-sm border border-gray-400">Fecha de pago</th>
-              <th className="px-2 py-2 text-sm border border-gray-400">Pagado</th>
-            </tr>
-            </thead>
-            <tbody>
-            {orderedBudgets().map((budget: Budget) => (
-                <tr key={budget.id}>
-                  <td className=" px-2 py-2 text-sm border border-gray-400 text-blue-700">{budget?.name}</td>
-                  <td className=" px-2 py-2 text-sm border border-gray-400">{(budget?.amount)?.toLocaleString('es-MX',{
-                    style: 'currency',
-                    currency: 'MXN'
+    <div className="flex flex-col">
+      {open && <BudgetModal setBudgets={setBudgets} creditCards={creditCards} open={open} onClose={() => {
+        setOpen(false)
+      }}/>}
+      {openGear && <BudgetConfigModal onClose={()=>{setOpenGear(false)}} show={openGear} />}
 
-                  })}</td>
-                  <td className=" px-2 py-2 text-sm border border-gray-400">{`${budget.pay_date}/${monthAndYear()}`}</td>
-                  <td className=" px-2 py-2 text-sm border border-gray-400 text-center">
-                    <input
-                      onChange={()=>{handleIsPaid(budget)}}
-                        className={`form-checkbox h-5 w-5 text-gray-600`}
-                        checked={budget?.paid} type="checkbox"/>
-                  </td>
-                </tr>
-
-            ))}
-
-            <tr>
-              <td className="px-2 py-2 text-sm border border-gray-400">Total</td>
-              <td className="px-2 py-2 text-sm border border-gray-400" >
-                {calculateTotal().toLocaleString('es-MX',{
-                    style: 'currency',
-                    currency: 'MXN'
-                    })
-                }
-              </td>
-              <td className=" px-2 py-2 text-sm border border-gray-400"></td>
-              <td className=" px-2 py-2 text-sm border border-gray-400"></td>
-            </tr>
-            </tbody>
-          </table>
-
-        </div>
-
+      <div className="flex flex-row">
+        <span className="font-bold text-xl">Presupuesto</span>
       </div>
+      <div className="flex flex-row justify-end gap-x-4 mt-12">
+        <button
+          onClick={() => setOpen(true)}
+          className="bg-gray-900 hover:bg-gray-800 text-white rounded-md px-4 py-2"
+        >
+          <FaGear className="text-white w-6 h-6"/>
+        </button>
+        <button
+          onClick={() => setOpen(true)}
+          className="bg-gray-900 hover:bg-gray-800 text-white rounded-md px-4 py-2"
+        >
+          + Agregar presupuesto
+        </button>
+      </div>
+      <div className="flex flex-row mt-12">
+        <table className="w-full">
+          <thead>
+          <tr>
+            <th className="px-2 py-2 text-sm border border-gray-400">Nombre</th>
+            <th className="px-2 py-2 text-sm border border-gray-400">Monto</th>
+            <th className="px-2 py-2 text-sm border border-gray-400">Fecha de pago</th>
+            <th className="px-2 py-2 text-sm border border-gray-400">Pagado</th>
+          </tr>
+          </thead>
+          <tbody>
+          {orderedBudgets().map((budget: Budget) => (
+            <tr key={budget.id}>
+              <td className=" px-2 py-2 text-sm border border-gray-400 text-blue-700">{budget?.name}</td>
+              <td className=" px-2 py-2 text-sm border border-gray-400">{(budget?.amount)?.toLocaleString('es-MX', {
+                style: 'currency',
+                currency: 'MXN'
+
+              })}</td>
+              <td className=" px-2 py-2 text-sm border border-gray-400">{`${budget.pay_date}/${monthAndYear()}`}</td>
+              <td className=" px-2 py-2 text-sm border border-gray-400 text-center">
+                <input
+                  onChange={() => {
+                    handleIsPaid(budget)
+                  }}
+                  className={`form-checkbox h-5 w-5 text-gray-600`}
+                  checked={budget?.paid} type="checkbox"/>
+              </td>
+            </tr>
+
+          ))}
+
+          <tr>
+            <td className="px-2 py-2 text-sm border border-gray-400">Total</td>
+            <td className="px-2 py-2 text-sm border border-gray-400">
+              {calculateTotal().toLocaleString('es-MX', {
+                style: 'currency',
+                currency: 'MXN'
+              })
+              }
+            </td>
+            <td className=" px-2 py-2 text-sm border border-gray-400"></td>
+            <td className=" px-2 py-2 text-sm border border-gray-400"></td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <hr className="my-4"/>
+      <span>Cálculo por Quincena y Mes</span>
+      <div className="flex flex-row justify-between px-4 md:px-8">
+        <span>1ra Quincena:</span>
+        <span>2ra Quincena:</span>
+      </div>
+
+    </div>
   );
 }
 
