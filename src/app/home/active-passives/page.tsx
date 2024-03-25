a508d5b5f8ce1769b05ed2b0f23462504df5df7b
josecarlospas1 <josecarlospas1@gmail.com>
2024-03-24 19:50:35 -0600

    feat(actives): add actives and pasives button

--- src/app/home/active-passives/page.tsx
+++ src/app/home/active-passives/page.tsx
@@ -1,9 +1,18 @@
+import {GiReceiveMoney} from "react-icons/gi";
+
 const ActivePassivesPage = () => {
   return(
     <div className="flex flex-col">
       <div className="flex flex-row">
         <span className="font-bold text-xl">Activos y pasivos</span>
       </div>
+        <div className="flex flex-row justify-center mt-60">
+            <div className="flex flex-col">
+                <GiReceiveMoney className="w-12 h-12 mx-auto"/>
+                <span className="font-bold text-xl">Aun no tienes activos ni pasivos</span>
+                <button className="bg-gray-900 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mt-4">Agregar activo / Pasivo</button>
+            </div>
+        </div>
     </div>
   );
 }
